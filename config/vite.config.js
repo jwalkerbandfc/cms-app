053806d96de02a749db666836e507729696c8113
jwalkerbandfc <143511732+jwalkerbandfc@users.noreply.github.com>
2026-09-03 15:03:41 +0100

    Refactor Vite config to use file and directory paths

--- config/vite.config.js
+++ config/vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite';
-import { glob } from 'glob';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   build: {
